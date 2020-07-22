655f5e13bf309ef94b5e784baefe38dacf32685a
Ashwin <chidambaramashwin@gmail.com>
2020-07-22 01:20:08 -0500

    SPI header file

---
+++ Wave_gen/Code/spi.h
@@ -0,0 +1,14 @@
+/* 
+ * File:   spi.h
+ * Author: hp
+ *
+ * Created on April 21, 2020, 12:08 AM
+ */
+
+#ifndef SPI__H__
+#define SPI__H__
+
+void initSPI();
+unsigned char spi_io(unsigned char o);
+
+#endif // SPI__H__
